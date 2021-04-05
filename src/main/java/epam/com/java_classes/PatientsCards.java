bce5326879246966b949783b844478d1078b5a63
Anastasiya Utsiuryna <anollad@tut.by>
2021-04-05 14:12:35 +0300

    move patient's initialization in main

--- src/main/java/epam/com/java_classes/PatientsCards.java
+++ src/main/java/epam/com/java_classes/PatientsCards.java
@@ -3,17 +3,18 @@ package epam.com.java_classes;
 import static epam.com.java_classes.Diagnosis.*;
 
     public class PatientsCards {
-        static Patient patient = new Patient("Ivan","I.", "Ivanov", 1, PNEUMONIA);
-        static Patient patient2 = new Patient("Petr", "P.","Petrov", 2, PNEUMONIA);
-        static Patient patient3 = new Patient("Olga", "S.","Sidorova", 3, FLU);
-        static Patient patient4 = new Patient("Maria", "D.","Dmitrieva", 4, HEART_ATTACK);
-        static Patient patient5 = new Patient("Fedor", "F","Fedorov", 5, DIABETES);
-        static Patient patient6 = new Patient("Anna", "P.","Pavlova", 6, DIABETES);
-        static Patient patient7 = new Patient("Yulia","D.", "Zaiceva", 7, GASTRITIS);
-
-        static Patient [] patients = new Patient[]{patient, patient2, patient3, patient4, patient5, patient6, patient7};
 
         public static void main(String[] args){
+
+            Patient patient = new Patient("Ivan","I.", "Ivanov", 1, PNEUMONIA);
+            Patient patient2 = new Patient("Petr", "P.","Petrov", 2, PNEUMONIA);
+            Patient patient3 = new Patient("Olga", "S.","Sidorova", 3, FLU);
+            Patient patient4 = new Patient("Maria", "D.","Dmitrieva", 4, HEART_ATTACK);
+            Patient patient5 = new Patient("Fedor", "F","Fedorov", 5, DIABETES);
+            Patient patient6 = new Patient("Anna", "P.","Pavlova", 6, DIABETES);
+            Patient patient7 = new Patient("Yulia","D.", "Zaiceva", 7, GASTRITIS);
+
+            Patient [] patients = new Patient[]{patient, patient2, patient3, patient4, patient5, patient6, patient7};
 
             PatientsResearch.getPatientsDiagnosis(FLU, patients);
             PatientsResearch.getPatientsMedicalCardNumber(1, 5, patients);
